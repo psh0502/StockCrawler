22f8c249ba6ec4858f046ff3bd8c251b8eda1b1f
Tom Tang <tom.tang@doway.com.tw>
2017-02-20 07:59:41 +0800

    Remove useless files

--- src/TomTang.Core/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace TomTang.Core
-{
-    public class Constants
-    {
-        public const string DOWILL_PUBLICTOKEN_KEY = "dce294cc33c2e22b";
-    }
-}
--- src/TomTang.Core/CopyrightViolationException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace TomTang.Core
-{
-    public class CopyrightViolationException : Exception
-    {
-        public CopyrightViolationException(string message) : base(message) { }
-    }
-}
