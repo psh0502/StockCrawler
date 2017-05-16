4ba8b96a49b25fe4d654ad424ad228b67c1025b9
Tom Tang <tom.tang@doway.com.tw>
2017-05-16 23:38:27 +0800

    Update sp script

--- database/MySQL/05.StoredProcedures/InsertOrUpdateStockList.StoredProcedure.sql
+++ database/MySQL/05.StoredProcedures/InsertOrUpdateStockList.StoredProcedure.sql
@@ -1,15 +1,18 @@
-CREATE PROCEDURE `InsertOrUpdateStockList` (
-IN stockId int, stockNo char, stockName char)
-BEGIN
-	INSERT INTO `stock`
-	(`StockID`,
-	`StockNo`,
-	`StockName`,
-	`Enable`)
-	VALUES
-	(stockId,
-	stockNo,
-	stockName,
-	true);
+DROP procedure IF EXISTS `DisableAllStocks`;
+DELIMITER $$
 
+CREATE PROCEDURE `InsertOrUpdateStockList`(
+stockNo varchar(10), stockName varchar(50))
+BEGIN
+	IF EXISTS(SELECT `StockID` FROM `stock` WHERE `StockNo` = stockNo) THEN
+		UPDATE `stock` SET `Enabled` = TRUE WHERE `StockNo` = stockNo;
+	ELSE
+		INSERT INTO `stock`
+		(`StockNo`,
+		`StockName`)
+		VALUES
+		(stockNo,
+		stockName);
+
+	END IF;
 END
