4a2813322dd833e560e3ea9439753d6b24aeb56c
Tom Tang <tomtang0406@gmail.com>
2017-05-31 00:46:52 +0800

    Parse csv data with ServiceStack.Text

--- src/StockCrawler/StockPriceHistoryInitJob.cs
+++ src/StockCrawler/StockPriceHistoryInitJob.cs
@@ -1,10 +1,11 @@
 ﻿using Common.Logging;
 using Quartz;
+using ServiceStack.Text;
 using StockCrawler.Dao;
 using StockCrawler.Dao.Schema;
 using System;
-using System.IO;
 using System.Net;
+using System.Text;
 
 namespace StockCrawler.Services
 {
@@ -34,75 +35,79 @@ namespace StockCrawler.Services
 
         private void downloadTwselatestInfo()
         {
-            using (WebClient wc = new WebClient())
+            byte[] downloaded_data = null;
+            using (var wc = new WebClient())
+                downloaded_data = wc.DownloadData(string.Format("http://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date={0}&type=ALLBUT0999", DateTime.Today.ToString("yyyyMMdd")));
+
+            string csv_data = Encoding.UTF8.GetString(downloaded_data);
+
+            // Usage of CsvReader: http://blog.darkthread.net/post-2017-05-13-servicestack-text-csvserializer.aspx
+            var csv_lines = CsvReader.ParseLines(csv_data);
+            var dt = new StockDataSet.StockDataTable();
+            bool found_stock_list = false;
+            foreach (var ln in csv_lines)
             {
-                var bin = wc.DownloadData(string.Format("http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php?download=csv&qdate={0}/{1}&selectType=ALLBUT0999", DateTime.Today.Year - 1911, DateTime.Today.ToString("MM/dd")));
-                using (StreamReader sr = new StreamReader(new MemoryStream(bin, false)))
+                //證券代號	證券名稱	成交股數	成交筆數	成交金額	開盤價	最高價	最低價	收盤價	漲跌(+/-)	漲跌價差	最後揭示買價	最後揭示買量	最後揭示賣價	最後揭示賣量	本益比
+                string[] data = CsvReader.ParseFields(ln).ToArray();
+                if (found_stock_list)
                 {
-                    var dt = new StockDataSet.StockDataTable();
-                    sr.ReadLine();
-                    while (true)
+                    if ("備註:" == data[0].Trim())
                     {
-                        string s = sr.ReadLine();
-                        if (string.IsNullOrEmpty(s)) break;
-                        string[] datas = s.Split(',');
-                        if (datas.Length == 7)
-                        {
-                            var dr = dt.NewStockRow();
-                            dr.StockDT = DateTime.Parse(datas[0]);
-                            dr.OpenPrice = decimal.Parse(datas[1]);
-                            dr.HighPrice = decimal.Parse(datas[2]);
-                            dr.LowPrice = decimal.Parse(datas[3]);
-                            dr.ClosePrice = decimal.Parse(datas[4]);
-                            dr.Volumn = long.Parse(datas[5]) / 1000;
-                            dr.AdjClosePrice = decimal.Parse(datas[6]);
-                            dr.StockID = stockID;
-                            dr.DateCreated = DateTime.Now;
-                            dt.AddStockRow(dr);
-                        }
+                        found_stock_list = false;
+                        break;
                     }
-                    StockDataService.GetServiceInstance().RenewStockList(dt);
+
+                    var dr = dt.NewStockRow();
+                    dr.StockNo = data[0].Trim();
+                    dr.StockName = data[1].Trim();
+                    dr.Enable = true;
+                    dr.DateCreated = DateTime.Now;
+                    dt.AddStockRow(dr);
+                }
+                else
+                {
+                    if ("證券代號" == data[0])
+                        found_stock_list = true;
                 }
             }
+
+            StockDataService.GetServiceInstance().RenewStockList(dt);
         }
 
         private void downloadYahooStockCSV(string stockNo, DateTime startDT, DateTime endDT, int stockID)
         {
-            byte[] csvBin = null;
+            byte[] downloaded_data = null;
             try
             {
-                using (WebClient wc = new WebClient())
-                    csvBin = wc.DownloadData(string.Format("http://ichart.finance.yahoo.com/table.csv?s={0}&d={1}&e={2}&f={3}&g=d&a={4}&b={5}&c={6}&ignore=.csv",
+                using (var wc = new WebClient())
+                    downloaded_data = wc.DownloadData(string.Format("http://ichart.finance.yahoo.com/table.csv?s={0}&d={1}&e={2}&f={3}&g=d&a={4}&b={5}&c={6}&ignore=.csv",
                         stockNo,
                         endDT.Day - 1, endDT.Month - 1, endDT.Year,
                         startDT.Day - 1, startDT.Month - 1, startDT.Year));
 
-                using (var sr = new StreamReader(new MemoryStream(csvBin, false)))
+                string csv_data = Encoding.UTF8.GetString(downloaded_data);
+                var csv_lines = CsvReader.ParseLines(csv_data);
+
+                var dt = new StockDataSet.StockPriceHistoryDataTable();
+                foreach (var ln in csv_lines)
                 {
-                    var dt = new StockDataSet.StockPriceHistoryDataTable();
-                    sr.ReadLine();
-                    while (true)
+                    string[] data = CsvReader.ParseFields(ln).ToArray();
+                    if (data.Length == 7)
                     {
-                        string s = sr.ReadLine();
-                        if (string.IsNullOrEmpty(s)) break;
-                        string[] datas = s.Split(',');
-                        if (datas.Length == 7)
-                        {
-                            var dr = dt.NewStockPriceHistoryRow();
-                            dr.StockDT = DateTime.Parse(datas[0]);
-                            dr.OpenPrice = decimal.Parse(datas[1]);
-                            dr.HighPrice = decimal.Parse(datas[2]);
-                            dr.LowPrice = decimal.Parse(datas[3]);
-                            dr.ClosePrice = decimal.Parse(datas[4]);
-                            dr.Volumn = long.Parse(datas[5]) / 1000;
-                            dr.AdjClosePrice = decimal.Parse(datas[6]);
-                            dr.StockID = stockID;
-                            dr.DateCreated = DateTime.Now;
-                            dt.AddStockPriceHistoryRow(dr);
-                        }
+                        var dr = dt.NewStockPriceHistoryRow();
+                        dr.StockDT = DateTime.Parse(data[0]);
+                        dr.OpenPrice = decimal.Parse(data[1]);
+                        dr.HighPrice = decimal.Parse(data[2]);
+                        dr.LowPrice = decimal.Parse(data[3]);
+                        dr.ClosePrice = decimal.Parse(data[4]);
+                        dr.Volumn = long.Parse(data[5]) / 1000;
+                        dr.AdjClosePrice = decimal.Parse(data[6]);
+                        dr.StockID = stockID;
+                        dr.DateCreated = DateTime.Now;
+                        dt.AddStockPriceHistoryRow(dr);
                     }
-                    StockDataService.GetServiceInstance().UpdateStockPriceHistoryDataTable(dt);
                 }
+                StockDataService.GetServiceInstance().UpdateStockPriceHistoryDataTable(dt);
             }
             catch (WebException wex)
             {
