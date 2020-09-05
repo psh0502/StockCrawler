568835978abc44588592143a0bb3e67b29b87915
Tom Tang <tomtang0406@gmail.com>
2020-09-05 16:38:59 +0800

    DataService adopt unity DI also

--- src/StockCrawler.Dao/StockDataServiceProvider.cs
+++ src/StockCrawler.Dao/StockDataServiceProvider.cs
@@ -1,14 +1,22 @@
-﻿namespace StockCrawler.Dao
+﻿using Microsoft.Practices.Unity.Configuration;
+using Unity;
+
+namespace StockCrawler.Dao
 {
     public abstract class StockDataServiceProvider
     {
+        private static readonly UnityContainer _container = new UnityContainer();
+        static StockDataServiceProvider()
+        {
+            _container.LoadConfiguration();
+        }
         /// <summary>
         /// Retrieve a new service instance. It's thread-safe.
         /// </summary>
         /// <returns>Database service instance</returns>
         public static IStockDataService GetServiceInstance()
         {
-            return new StockDataServiceMSSQL();
+            return _container.Resolve<IStockDataService>();
         }
     }
 }
