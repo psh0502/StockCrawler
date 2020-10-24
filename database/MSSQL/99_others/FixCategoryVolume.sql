ff96e9ac5f48bbfecb281ad148e19804cdff4337
Tom Tang <tomtang0406@gmail.com>
2020-10-24 09:49:19 +0800

    #24 Adjust data patching sql script

--- database/MSSQL/99_others/FixCategoryVolume.sql
+++ database/MSSQL/99_others/FixCategoryVolume.sql
@@ -17,7 +17,8 @@ UPDATE StockPriceHistory
 SET Volume = 
 (CASE StockNo 
 	WHEN '0009' THEN T.TotalVolume - T.FinancialTotalAmount 
-	WHEN '0010' THEN T.TotalVolume - T.ElectronicTotalAmount 
+	WHEN '0010' THEN T.TotalVolume - T.ElectronicTotalAmount
+	WHEN '0011' THEN T.TotalVolume - T.FinancialTotalAmount - T.ElectronicTotalAmount
 END)
 FROM
 	StockPriceHistory a(NOLOCK)
@@ -29,5 +30,5 @@ FROM
 			INNER JOIN StockPriceHistory d(NOLOCK) ON d.StockDt = b.StockDT AND d.[Period] = b.[Period] AND d.StockNo BETWEEN '0029' AND '0036'
 		WHERE b.[Period] = 1 AND b.StockNo = '0000'
 		GROUP BY b.StockDT, b.Volume, c.Volume) T ON a.StockDT = T.StockDT
-WHERE [Period] = 1 AND StockNo IN('0009', '0010')
+WHERE [Period] = 1 AND StockNo IN('0009', '0010', '0011')
 GO
