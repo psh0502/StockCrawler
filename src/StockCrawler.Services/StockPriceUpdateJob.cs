073178fe232aa4b5f8491eaab44d71d0423ea271
Tom Tang <tom.tang@doway.com.tw>
2021-01-30 23:21:05 +0800

    enhance job flow

--- src/StockCrawler.Services/StockPriceUpdateJob.cs
+++ src/StockCrawler.Services/StockPriceUpdateJob.cs
@@ -2,7 +2,6 @@
 using Quartz;
 using StockCrawler.Dao;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -22,34 +21,23 @@ namespace StockCrawler.Services
             {
                 var collector = CollectorServiceProvider.GetStockDailyPriceCollector();
 
-                var list = new List<GetStockPeriodPriceResult>();
+                var priceInfo = collector.GetStockDailyPriceInfo();
+                var stocks = priceInfo
+                    .Select(d => new GetStocksResult()
+                    {
+                        StockNo = d.StockNo,
+                        StockName = d.StockName,
+                        Enable = true
+                    }).ToList();
+
                 using (var db = GetDB())
                 {
-                    var stocks = collector.GetStockDailyPriceInfo()
-                        .Select(d => new GetStocksResult()
-                        {
-                            StockNo = d.StockNo,
-                            StockName = d.StockName,
-                            Enable = true
-                        }).ToList();
-
                     if (stocks.Any())
                         db.InsertOrUpdateStock(stocks.ToArray());
 
-                    foreach (var d in collector.GetStockDailyPriceInfo())
-                    {
-                        Logger.DebugFormat("Retrieving daily price of [{0}] stock.", d.StockNo);
-                        var info = collector.GetStockDailyPriceInfo(d.StockNo);
-                        if (null != info)
-                        {
-                            db.InsertOrUpdateStock(info.StockNo, info.StockName, null);
-                            list.Add(info);
-                            Logger.InfoFormat("Finish the {0} stock daily price retrieving task.", d.StockNo);
-                        }
-                    }
-                    if (list.Any())
+                    if (priceInfo.Any())
                         // 寫入日價
-                        db.InsertOrUpdateStockPrice(list.ToArray());
+                        db.InsertOrUpdateStockPrice(priceInfo.ToArray());
                 }
                 Tools.CalculateMAAndPeriodK(SystemTime.Today);
             }
